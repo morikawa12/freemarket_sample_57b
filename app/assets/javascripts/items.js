8024ec54d3a38d480608372c94c9c7788bc516ce
yamachan0625 <napoli.080625@gmail.com>
2019-11-04 20:35:54 +0900

    divide-file

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,6 +1,12 @@
 $(document).on('turbolinks:load', function(){
-  $('.slider').slick({
+  $('.slider').not('.slick-initialized').slick({
     autoplay:true,
+    autoplaySpeed:4000,
     dots:true,
+    dotsClass:'slide-dots'
+  });
+
+  $('.footer-mercari-logo').click(function(){
+    $('body, html').scrollTop(0);
   });
 });
