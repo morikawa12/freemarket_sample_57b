5eb57a176dd90fdcdd443c195a266c7c58e85005
ruorinshin960 <ruorin.sin960za@gmail.com>
2019-11-09 15:29:57 +0900

    registration_view&scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'comments/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'comments#index' 
+  get 'comments/index' 
 end
