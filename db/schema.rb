df02acb91c2e8212c212f96f7174eb924a198cbb
ruorinshin960 <ruorin.sin960za@gmail.com>
2019-11-10 13:22:59 +0900

    fixed conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_115221) do
+
+ActiveRecord::Schema.define(version: 2019_11_02_920420) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "status", null: false
+    t.integer "prefecture", null: false
+    t.integer "fee", null: false
+    t.integer "size", null: false
+    t.integer "shipping", null: false
+    t.integer "arrival", null: false
+    t.integer "like", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name"
+
+ActiveRecord::Schema.define(version: 2019_11_09_115957) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.integer "birth_year", null: false
+    t.integer "birth_month", null: false
+    t.integer "birth_day", null: false
+    t.string "mobile_phone", null: false
+    t.string "profile_image"
+    t.text "profile_content"
+    t.string "card_number"
+    t.string "expiration_month"
+    t.string "expiration_year"
+    t.string "security_code"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +64,5 @@ ActiveRecord::Schema.define(version: 2019_10_30_115221) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
