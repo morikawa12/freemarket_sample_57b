9fed93a127f8967c3f960320088e40e398d96cb5
小堺　篤 <kozakaiatsushi@kosakaiatsushinoMacBook-Pro.local>
2019-11-10 13:36:52 +0900

    create-addresses-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_115221) do
+ActiveRecord::Schema.define(version: 2019_11_10_033820) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "relative_family_name", null: false
+    t.string "relative_first_name", null: false
+    t.string "relative_family_name_kana", null: false
+    t.string "relative_first_name_kana", null: false
+    t.string "zip_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building", null: false
+    t.string "home_phone", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
