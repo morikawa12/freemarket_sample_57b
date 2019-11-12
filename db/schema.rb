00165c4a50bf044d7d21061778b1042c8778598f
yamachan0625 <napoli.080625@gmail.com>
2019-11-13 02:42:33 +0900

    prefectures-table-drop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_143048) do
+ActiveRecord::Schema.define(version: 2019_11_12_174116) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "relative_family_name", null: false
@@ -71,11 +71,6 @@ ActiveRecord::Schema.define(version: 2019_11_12_143048) do
     t.index ["name"], name: "index_items_on_name"
     t.index ["size_id"], name: "index_items_on_size_id"
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
