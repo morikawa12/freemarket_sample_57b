723bd92ab3eabbabd3a252bd44454fc0fa6244cf
yamachan0625 <napoli.080625@gmail.com>
2019-11-13 21:27:19 +0900

    edit-items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,24 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @parents = Category.all.order("id ASC").limit(13)
+    @item.build_brand
+    @item.build_size
+    @item.build_category
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :status, :prefecture, :fee, :arrival,
+    size_attributes: [:id, :size, :category_id],
+    brand_attributes: [:id, :name],
+    category_attributes: [:id, :name],
+    images_attributes: [:id, :image_url])
+  end
+
 end
