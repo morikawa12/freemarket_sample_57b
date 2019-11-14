9587ab9503a23dc076cc643b17a9e30240077a3c
yamachan0625 <napoli.080625@gmail.com>
2019-11-14 19:11:52 +0900

    add-size-ancestry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_044936) do
+ActiveRecord::Schema.define(version: 2019_11_14_100716) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "relative_family_name", null: false
@@ -98,6 +98,8 @@ ActiveRecord::Schema.define(version: 2019_11_14_044936) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_sizes_on_ancestry"
     t.index ["category_id"], name: "index_sizes_on_category_id"
   end
 
