45fef78e8ebf1d658ab28d58ffd2c1b99c000892
Daiki.Ishikawa <ishikawadaiki@gmail.com>
2019-11-16 20:28:40 +0900

    Update to redirect users_omuniauth_callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # Facebook側で認証情報が挿入されると、再度リダイレクトしてアプリケーションのコールバックメソッドに戻ります。
+  # コールバックを実装する場合に最初に必要なのは、config/routes.rbを開いて、Omniauthコールバックを実装するコールバックをDeviseに伝えることです
   
   root 'items#index'
   resources :items, only: [:index]
