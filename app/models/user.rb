812f87930e620dd9f51d4c124a8fc5be12fbe52c
Daiki.Ishikawa <ishikawadaiki@gmail.com>
2019-11-21 22:55:56 +0900

    Delete_unnecesary columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,6 @@ class User < ApplicationRecord
           nickname: auth.info.name,
           email:    auth.info.email,
           password: Devise.friendly_token[0, 20],
-          telephone: "08000000000"
           )
         SnsCredential.create(
           uid: uid,
