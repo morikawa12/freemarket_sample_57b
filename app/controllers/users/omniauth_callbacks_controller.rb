000fd085081322e0adc3d82592f9ec9b6db61bee
Daiki.Ishikawa <ishikawadaiki@gmail.com>
2019-11-27 21:09:08 +0900

    Delete_UnnecessaryCode

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,17 +10,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
   
   def callback_for(provider)
-    # FacebookからOmniAuthで取得したすべての情報はrequest.env["omniauth.auth"]のハッシュでアクセスできます
     @user = User.find_oauth(request.env["omniauth.auth"])
-    # @userが保存済みか確認
     if @user.persisted?
-      # sign_in_and_redirect @user, event: :authentication #after_sign_in_path_forと同じパス
       sign_in @user
       redirect_to root_path
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
-      # OmniAuthで所得したデータをprovider_dataに格納し、セッションで引き回せるようにしてリダイレクト
-      # devise.をつけておくとdeviseでログインした後に捨てるから安全らしい
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_path
     end
@@ -30,15 +25,3 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path
   end  
 end
-  # def facebook
-  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-  #     set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-  #   else
-  #     session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
