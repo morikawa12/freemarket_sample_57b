8a07aca911f2f6bc1d8cc1a5570148351fe9da2a
yamachan0625 <napoli.080625@gmail.com>
2019-12-02 19:39:47 +0900

    change-images-post-function

--- app/assets/javascripts/image_upload.js
+++ app/assets/javascripts/image_upload.js
@@ -1,19 +1,144 @@
 $( document ).on('turbolinks:load', function() {
 
-  function readURL(input) {
+  function readURL1(input) {
     if (input.files && input.files[0]) {
       var reader = new FileReader();
 
       reader.onload = function (e) {
-        $('#img_url_prev').attr('src', e.target.result);
+        $('.img_url_prev1').attr('src', e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
     }
   }
 
-  $(document).on('change','#item_images_attributes_0_image',function(e){
-    readURL(this);
-    $('.exhibition_drop_upload_box').css('width','80%');
-    $('.image_upload_field').css('display','block')
+  function readURL2(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('.img_url_prev2').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  function readURL3(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('.img_url_prev3').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  function readURL4(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('.img_url_prev4').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  function readURL5(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('.img_url_prev5').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+
+
+  $('.image_upload_field__delete-btn1').on('click', function(){
+    $('.image_upload_btn1').val('');
+    $('.image_upload_btn01').val('');
+    $('.image_upload_field1').css('display','none');
+  })
+
+  $('.image_upload_field__delete-btn2').on('click', function(){
+    $('.image_upload_btn2').val('');
+    $('.image_upload_btn02').val('');
+    $('.image_upload_field2').css('display','none');
+  })
+
+  $('.image_upload_field__delete-btn3').on('click', function(){
+    $('.image_upload_btn3').val('');
+    $('.image_upload_btn03').val('');
+    $('.image_upload_field3').css('display','none');
+  })
+
+  $('.image_upload_field__delete-btn4').on('click', function(){
+    $('.image_upload_btn4').val('');
+    $('.image_upload_btn04').val('');
+    $('.image_upload_field4').css('display','none');
+  })
+
+  $('.image_upload_field__delete-btn5').on('click', function(){
+    $('.image_upload_btn5').val('');
+    $('.image_upload_btn05').val('');
+    $('.image_upload_field5').css('display','none');
+  })
+
+  $(document).on('change','.image_upload_btn1',function(e){
+    readURL1(this);
+    $('.exhibition_drop_upload_box1').css('display','none');
+    $('.image_upload_field1').css('display','block');
+    $('.exhibition_drop_upload_box2').css('display','block');
+  })
+  
+
+  $(document).on('change','.image_upload_btn2',function(e){
+    readURL2(this);
+    $('.exhibition_drop_upload_box2').css('display','none');
+    $('.image_upload_field2').css('display','block');
+    $('.exhibition_drop_upload_box3').css('display','block');
+  })
+
+  $(document).on('change','.image_upload_btn3',function(e){
+    readURL3(this);
+    $('.exhibition_drop_upload_box3').css('display','none');
+    $('.image_upload_field3').css('display','block');
+    $('.exhibition_drop_upload_box4').css('display','block');
+  })
+  
+  $(document).on('change','.image_upload_btn4',function(e){
+    readURL4(this);
+    $('.exhibition_drop_upload_box4').css('display','none');
+    $('.image_upload_field4').css('display','block');
+    $('.exhibition_drop_upload_box5').css('display','block');
+  })
+
+  $(document).on('change','.image_upload_btn5',function(e){
+    readURL5(this);
+    $('.exhibition_drop_upload_box').css('display','none');
+    $('.image_upload_field5').css('display','block');
+  })
+
+  $(document).on('change','.image_upload_btn01',function(e){
+    readURL1(this);
+  })
+
+  $(document).on('change','.image_upload_btn02',function(e){
+    readURL2(this);
+  })
+
+  $(document).on('change','.image_upload_btn03',function(e){
+    readURL3(this);
+  })
+
+  $(document).on('change','.image_upload_btn04',function(e){
+    readURL4(this);
+  })
+
+  $(document).on('change','.image_upload_btn05',function(e){
+    readURL5(this);
   })
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -90,7 +90,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :description, :status, :prefecture, :fee, :arrival, :category_id, :size_id,:shipping_id,:product_status,:user_id,:brand_id,
     brand_attributes: [:id, :name],
-    images_attributes: [:id, :image])
+    images_attributes: [:id, :image, :_destroy])
   end
 
   def set_parents
