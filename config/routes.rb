f36db04c50aec7a4382342da2ba3273c22aac655
yamachan0625 <napoli.080625@gmail.com>
2019-12-07 02:09:11 +0900

    add-routeing

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,19 @@ Rails.application.routes.draw do
   root 'items#index'
 
 
-  resources :items, only: [:index, :new, :create] do
+  resources :items, only: [:index, :new, :create, :buy] do
   collection do
     get 'get_category_children', defaults: { format: 'json' }
     get 'get_category_grandchildren', defaults: { format: 'json' }
     get 'get_size', defaults: { format: 'json' }
     get 'get_shipping', defaults: { format: 'json' }
+  end
+  member do
+    get 'get_category_children', defaults: { format: 'json' }
+    get 'get_category_grandchildren', defaults: { format: 'json' }
+    get 'get_size', defaults: { format: 'json' }
+    get 'get_shipping', defaults: { format: 'json' }
+    get 'buy', to: 'items#buy'
   end
 end
 
