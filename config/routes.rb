25cf3a7c8ceca0330b528d36cb14d7e1e15aa211
yamachan0625 <napoli.080625@gmail.com>
2019-12-18 18:28:38 +0900

    edit-route-buy

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,9 @@ Rails.application.routes.draw do
 
 
 
-  resources :items, only: [:index, :new, :create, :edit, :update, :buym, :show] do
-
+  resources :items, only: [:index, :new, :create, :edit, :update, :show] do
   collection do
+    get 'buy'
     get 'get_category_children', defaults: { format: 'json' }
     get 'get_category_grandchildren', defaults: { format: 'json' }
     get 'get_size', defaults: { format: 'json' }
