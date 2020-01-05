59741e11b182449b7c2aea20f4132a68ca89a115
yamachan0625 <napoli.080625@gmail.com>
2020-01-05 12:25:21 +0900

    changed from where to fincd_by

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -28,7 +28,7 @@ class CardController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除します
-    card = Card.where(user_id: current_user.id).first
+    card = Card.find_by(user_id: current_user.id)
     if card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
@@ -40,7 +40,7 @@ class CardController < ApplicationController
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
+    card = Card.find_by(user_id: current_user.id)
     if card.blank?
       redirect_to action: "new" 
     else
