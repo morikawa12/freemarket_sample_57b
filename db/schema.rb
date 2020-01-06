918ee17bca46ef881c357753265da09ca697f524
ruorinshin960 <ruorin.sin960za@gmail.com>
2020-01-07 01:22:32 +0900

    create pull request

--- db/schema.rb
+++ db/schema.rb
@@ -115,6 +115,15 @@ ActiveRecord::Schema.define(version: 2020_01_03_162817) do
     t.index ["category_id"], name: "index_sizes_on_category_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -139,4 +148,5 @@ ActiveRecord::Schema.define(version: 2020_01_03_162817) do
   add_foreign_key "items", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "sizes", "categories"
+  add_foreign_key "sns_credentials", "users"
 end
