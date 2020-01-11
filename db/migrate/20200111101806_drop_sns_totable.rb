e1e0cc804a7700a547776c770a42c5d26709f9de
ruorinshin960 <ruorin.sin960za@gmail.com>
2020-01-11 19:19:52 +0900

    fixed sns-table

---
+++ db/migrate/20200111101806_drop_sns_totable.rb
@@ -0,0 +1,5 @@
+class DropSnsTotable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :sns_credentials
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_120914) do
+ActiveRecord::Schema.define(version: 2020_01_11_101806) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -112,15 +112,6 @@ ActiveRecord::Schema.define(version: 2020_01_08_120914) do
     t.index ["category_id"], name: "index_sizes_on_category_id"
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -145,5 +136,4 @@ ActiveRecord::Schema.define(version: 2020_01_08_120914) do
   add_foreign_key "items", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "sizes", "categories"
-  add_foreign_key "sns_credentials", "users"
 end
